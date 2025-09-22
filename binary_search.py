fed7804f80414fa62bbe2e7d7f59dc66901b319d
Anam Shaikh <anamashaikh2004@gmail.com>
2025-09-22 20:30:58 +0530

    Binary search but return the index such that it is small if repetition is present.

---
+++ binary_search.py
@@ -0,0 +1,18 @@
+#Search on what index element is found and If multiple occurrences are there, please return the smallest index.
+class Solution:
+    def binarysearch(self, arr, k):
+        st=0
+        end=len(arr)-1
+        ans=-1
+        while(st<=end):
+            mid=(st+end)//2
+            if arr[mid]==k:
+                ans=mid
+                end=mid-1
+            elif arr[mid]<k:
+                st=mid+1
+            else:
+                end=mid-1
+        return ans
+s=Solution()
+print(s.binarysearch([1,8,6,7,6,9],6))
